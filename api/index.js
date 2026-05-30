b94cffc920ae00b69c4ae0baaf3dc7d80d203ec7
accountsource <clicoooc@gmail.com>
2026-05-30 22:00:38 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
-// api/index.js
-
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/pdf');
 
   res.status(302);
   res.setHeader('Location', 'https://teka.eu.org');
 
-  res.end();
+  res.end(`
+%PDF-1.4
+1 0 obj
+<<>>
+endobj
+trailer
+<<>>
+%%EOF
+`);
 }
